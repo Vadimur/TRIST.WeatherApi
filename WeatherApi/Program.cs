3b66bacd56a2ca01d6cbac968334cf41b0486a90
vadym.mulish <vadym.mulish@gr8.tech>
2024-12-27 15:53:31 +0200

    init commit

---
+++ WeatherApi/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Serilog.Sinks.Fluentd;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog((ctx, config) =>
+{
+    config.ReadFrom.Configuration(ctx.Configuration);
+    
+    config
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.Fluentd(new FluentdSinkOptions("localhost", 24224))
+        .WriteTo.Console();
+});
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", ([FromServices] ILogger<Program> logger) =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        
+        
+        logger.LogInformation("Hello from manual log. Current UTC time: {Time}", DateTime.UtcNow);
+        
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
